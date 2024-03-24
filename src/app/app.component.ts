a0957c0603ae929a9e89e112472abf2be938d8d5
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-03-23 20:24:49 -0500

    (UPDATE) Image background is ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from "./head/head.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeadComponent,
+      CommonModule]
 })
 export class AppComponent {
   title = 'hostal-casa-madrigal';
---
+++ src/app/head/head.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './head.component.html',
+  styleUrl: './head.component.css'
+})
+export class HeadComponent {
+
+}
