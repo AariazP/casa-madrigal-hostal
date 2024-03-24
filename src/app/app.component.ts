878c4be2ccfa1ade94ed940c3d567e56e9ac4964
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-03-23 21:18:54 -0500

    Se inicia con el nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadComponent } from "./head/head.component";
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeadComponent,
-      CommonModule]
+    imports: [RouterOutlet, 
+      HeadComponent,
+      CommonModule,
+      NavbarComponent,
+      HeadComponent
+    ]
 })
 export class AppComponent {
   title = 'hostal-casa-madrigal';
--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-  selector: 'app-head',
-  standalone: true,
-  imports: [
-    CommonModule
-  ],
-  templateUrl: './head.component.html',
-  styleUrl: './head.component.css'
+    selector: 'app-head',
+    standalone: true,
+    templateUrl: './head.component.html',
+    styleUrl: './head.component.css',
+    imports: [
+        CommonModule,
+        NavbarComponent
+    ]
 })
 export class HeadComponent {
 
