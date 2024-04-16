a30668e137e5c9c061b12a9801e419cafc32e7de
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-04-16 08:08:26 -0500

    (UPDATE) Add ngSrc to carrousel

--- src/app/carrousel/carrousel.component.ts
+++ src/app/carrousel/carrousel.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 @Component({
   selector: 'app-carrousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './carrousel.component.html',
   styleUrl: './carrousel.component.css'
 })
